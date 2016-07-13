1b432ef2ead105eaadb80c4b334a8787cf88741c
Cezary Krzyżanowski <cezary@thezenofit.com>
2016-07-13 09:43:38 +0200

    Initial example.

---
+++ StubbornIcon/StubbornIconPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace StubbornIcon
+{
+	public partial class StubbornIconPage : ContentPage
+	{
+		public StubbornIconPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
